9c56dcdde40465f87f062d63f68ef2af5c5014ec
Jay Dadhaniya <jaydadhaniya45@gmail.com>
2023-12-30 21:09:16 +0530

    Added game board component with all the logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,34 +1,38 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
     setPlayerName(event.target.value);
   }
 
-  let editablePlayer = <span className="player-name">{playerName}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    editablePlayer = (
-      <input
-        type="text"
-        required
-        value={playerName}
-        onChange={handleChange}
-      ></input>
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
-        {editablePlayer}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
